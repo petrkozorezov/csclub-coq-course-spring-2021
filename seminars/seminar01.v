0c6a4c53433b3c07a6c1bd03ecfce230bca83204
Petr Kozorezov <petr.kozorezov@gmail.com>
2021-03-20 21:34:12 +0300

    add a solving for seminar 1

--- seminars/seminar01.v
+++ seminars/seminar01.v
@@ -5,41 +5,68 @@ From mathcomp Require Import ssreflect.
 Definition prodA (A B C : Type) :
   (A * B) * C -> A * (B * C)
 :=
+  fun '((a, b), c) => (a, (b, c)).
 
 Definition sumA (A B C : Type) :
   (A + B) + C -> A + (B + C)
 :=
+  fun s =>
+    match s with
+    | inr c       => inr (inr c)
+    | inl (inl a) => inl a
+    | inl (inr b) => inr (inl b)
+    end.
 
 Definition prod_sumD (A B C : Type) :
   A * (B + C) -> (A * B) + (A * C)
-:=
+:= fun p =>
+  match p with
+    | (a, inl b) => inl (a, b)
+    | (a, inr c) => inr (a, c)
+  end.
+
 
 Definition sum_prodD (A B C : Type) :
   A + (B * C) -> (A + B) * (A + C)
+:= fun p =>
+  match p with
+  | inl a      => (inl a, inl a)
+  | inr (b, c) => (inr b, inr c)
+  end.
+
+Definition comp (A B C : Type) (f : B -> A) (g : C -> B) : C -> A
 :=
+  fun c => f (g c).
 
 
-(* function composition *)
-Definition comp A B C (f : B -> A) (g : C -> B) : C -> A
-:=
-
+(*Definition comp (A B C : Type) : (B -> A) -> (C -> B) -> C -> A
+:= fun f g => fun c => f (g c).
+*)
 
 (* Introduce a notation that lets us use composition like so: f \o g.
    You might need to tweak the implicit status of some comp's arguments *)
 
-
 (* Introduce empty type, call `void` *)
+
+Inductive void := .
 
 Definition void_terminal (A : Type) :
   void -> A
-:=
+:= fun p => match p with end.
 
 
 (* Introduce `unit` type (a type with exactly one canonical form) *)
 
+Inductive unit : Type :=
+  | u.
+
 Definition unit_initial (A : Type) :
   A -> unit
-:=
+:= fun '_ => u.
 
+Variable x : void.
+Check match x with end : nat.
+
+Compute void_terminal _ x : nat.
 
 (* Think of some more type signatures involving void, unit, sum, prod *)
